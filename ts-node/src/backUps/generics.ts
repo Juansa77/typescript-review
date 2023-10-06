a3e76bf629d92c0d9c9c098aeae57ba1056401c7
Juansa77 <dedalus1977@gmail.com>
2023-10-06 10:45:46 +0200

    ·added https mapped interfaces

---
+++ ts-node/src/backUps/generics.ts
@@ -0,0 +1,23 @@
+import { Villians } from './../interfaces/villians';
+import { Hero } from '../interfaces/heroes';
+import { genericFunction, genericFunctionArrow, printObject } from "../generics/generics";
+
+printObject(123)
+printObject(new Date())
+printObject({a:1, b:2})
+printObject([1,2,3,4,5])
+printObject("Hello")
+
+//* Ya que la función es genérica, tenemos acceso los métodos propios de cada tipo de dato de salida
+console.log(genericFunction(12).toFixed(2))
+console.log(genericFunction(new Date().getMonth))
+
+const deadPool ={
+    name:"Deadpool",
+    realName: "Wade Wilson",
+    dangerLevel: 130
+}
+
+//* Aquí hemos definido en la función que el argumento tiene que ser la interfaz Hero
+
+console.log(genericFunctionArrow<Hero>(deadPool))
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,25 +1,10 @@
-import { Hero } from "./interfaces/heroes";
-import { genericFunction, genericFunctionArrow, printObject } from "./generics/generics";
-import { Villians } from './interfaces/villians';
+import { Pokemon } from './interfaces/pokemon';
+import axios from "axios";
+import { getPokemon } from "./generics/get-pokemon";
 
+getPokemon(1)
+  .then(pokemon => console.log(pokemon.moves))
+  .catch(error => console.log(error))
+  .finally(() => console.log("fin de función"));
 
-
-printObject(123)
-printObject(new Date())
-printObject({a:1, b:2})
-printObject([1,2,3,4,5])
-printObject("Hello")
-
-//* Ya que la función es genérica, tenemos acceso los métodos propios de cada tipo de dato de salida
-console.log(genericFunction(12).toFixed(2))
-console.log(genericFunction(new Date().getMonth))
-
-const deadPool ={
-    name:"Deadpool",
-    realName: "Wade Wilson",
-    dangerLevel: 130
-}
-
-//* Aquí hemos definido en la función que el argumento tiene que ser la interfaz Hero
-
-console.log(genericFunctionArrow<Hero>(deadPool))
+console.log(getPokemon(2));
